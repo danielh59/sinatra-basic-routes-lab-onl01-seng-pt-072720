36cbf68f708eff7cc8624bd90a33a4a1c8f25e20
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-19 22:12:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Blank"
+  end
+
+  get '/hometown' do
+  "My hometown is blank"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is blank"
+  end
+
 end
